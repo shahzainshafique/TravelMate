289619d2f921f1282a7a100dd46b007d943ec3cf
Shahzain Shafique <shahzainshafique@pk.folio3.com>
2024-03-30 04:51:30 +0500

    Navbar and Hero

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,22 +5,24 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className=" flexBetween max-containter padding-container relative z-10 py-5">
-      <Link href={"/"}>
-        <Image src={"/hilink-logo.svg"} alt="Logo" width={74} height={29} />
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
+
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
             key={link.key}
-            className="regular-16 text-gray-600 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
           >
             {link.label}
           </Link>
         ))}
       </ul>
-      <div className="hidden lg:flexCenter">
+
+      <div className="lg:flexCenter hidden">
         <Button
           type="button"
           title="Login"
@@ -28,12 +30,13 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
+
       <Image
-        src={"/menu.svg"}
+        src="menu.svg"
         alt="menu"
         width={32}
         height={32}
-        className="inline-block lg:hidden cursor-pointer"
+        className="inline-block cursor-pointer lg:hidden"
       />
     </nav>
   );
